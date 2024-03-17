0053940bdf46b15c386b86c643081b26f7660ef7
Yatsk19 <ventusvitalik170@gmail.com>
2024-03-18 01:26:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,103 @@
+//public class Main {
+   // public static void main(String[] args) {
+        // Використовуємо цикл for для виведення чисел від 1 до 5
+        //for (int i = 1; i <= 5; i++) {
+          //  System.out.println(i);
+        //}
+    //}
+//}
+
+//#2
+//public class Main {
+   // public static void main(String[] args) {
+        //// Використовуємо цикл for для виведення чисел від 5 до 1
+        //for (int i = 5; i >= 1; i--) {
+            //System.out.println(i);
+        //}
+    //}
+//}
+
+//#3
+
+//public class Main {
+  //  public static void main(String[] args) {
+        // Виведення заголовка таблиці
+    //    System.out.println("Таблиця множення:");
+
+        // Вкладений цикл для обходу рядків та стовпців таблиці
+      //  for (int i = 1; i <= 10; i++) {
+        //    for (int j = 1; j <= 10; j++) {
+                // Обчислення добутку та його виведення
+          //      int product = i * j;
+            //    System.out.print(i + " * " + j + " = " + product + "\t");
+          //  }
+            // Перехід на новий рядок після виведення всіх значень для поточного рядка
+          //  System.out.println();
+      //  }
+   // }
+//}
+
+//#4
+
+//import java.util.Scanner;
+
+//public class Main {
+  //  public static void main(String[] args) {
+        // Створення об'єкту Scanner для отримання введення від користувача
+    //    Scanner scanner = new Scanner(System.in);
+
+        // Запит на введення числа від користувача
+      //  System.out.print("Введіть позитивне число: ");
+       // int number = scanner.nextInt();
+
+        // Перевірка, що введене число дійсно позитивне
+      //  if (number <= 0) {
+        //    System.out.println("Ви ввели непозитивне число. Будь ласка, введіть позитивне число.");
+          //  return;
+       // }
+
+        // Підсумовуємо всі числа від 1 до введеного числа
+       // int sum = 0;
+       // for (int i = 1; i <= number; i++) {
+         //   sum += i;
+       // }
+
+        // Виведення суми на екран
+      //  System.out.println("Сума чисел від 1 до " + number + " дорівнює: " + sum);
+   // }
+//}
+
+//#5
+
+//import java.util.Scanner;
+
+//public class Main {
+  //  public static void main(String[] args) {
+    //    Scanner scanner = new Scanner(System.in);
+
+      //  int num1, num2, product;
+       // char choice;
+
+       // do {
+
+         //   System.out.print("Введіть перше число: ");
+          //  num1 = scanner.nextInt();
+
+           // System.out.print("Введіть друге число: ");
+           // num2 = scanner.nextInt();
+
+
+           // product = num1 * num2;
+
+
+          //  System.out.println("Результат множення: " + product);
+
+           // System.out.print("Бажаєте продовжити виконання програми? Введіть 1 для завершення: ");
+           // choice = scanner.next().charAt(0);
+       // } while (choice != '1');
+
+       // System.out.println("Програма завершена.");
+   // }
+//}
+
+
